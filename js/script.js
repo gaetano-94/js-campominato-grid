65756a539fe537fcd79a2380e86afe84b405c523
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-15 16:45:05 +0100

    feat: prova js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//funzione
+function myCreateElement(tag, className, content) {
+  const element = document.createElement(tag);
+  element.classlist.add(className);
+  element.append(content);
+
+  return element;
+}
+
+//variabili
+const table = document.querySelector('.table');
+console.log(table);
+
+//ciclo
+for (let i = 1; i <= 100; i++) {
+  const myElement = myCreateElement('div', 'cell', i);
+  table.append(myElement);
+}
